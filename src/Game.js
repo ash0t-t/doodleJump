c0bf2532c38a5b445b6a26c3c6662d32892bd8a4
ash0t-t <ashottovmasyan2005@gmail.com>
2024-05-19 11:33:44 +0400

    added improvments

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,10 @@
 import { ctx, canvas } from './util.js'
+import { Decoration } from './Decoration.js'
 
 export class Game {
+    objects = [
+        new Decoration(600, 600, 600, 800, 'images/background.png')
+    ]
     constructor() {
         this.id = requestAnimationFrame(() => this.run())
     }
@@ -8,5 +12,6 @@ export class Game {
     run() {
         this.id = requestAnimationFrame(() => this.run())
         ctx.clearRect(0, 0, canvas.width, canvas.height)
+        this.objects.forEach(p => p.draw())
     }
 }
--- src/util.js
+++ src/util.js
@@ -1,2 +1,6 @@
 export const canvas = document.querySelector("canvas")
 export const ctx = canvas.getContext("2d")
+
+canvas.style = "position: absolute; top: 100px; left: 100px; right: 100px; bottom: 100px; margin: auto; border:2px solid tan";
+canvas.width = 600
+canvas.height = 800
